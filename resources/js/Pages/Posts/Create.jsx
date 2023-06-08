a5c496655e568a3c2030822bd2aed78b6acd568c
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2023-06-08 13:56:27 +0700

    view, ok

---
+++ resources/js/Pages/Posts/Create.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+// import layout
+import Layout from "../../Layouts/Default";
+
+// import inertia adapter
+import { Inertia } from "@inertiajs/inertia";
+
+export default function createPost({ errors }) {
+    // define state
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    // function "storePost"
+}
